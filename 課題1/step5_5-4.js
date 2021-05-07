19aeea42b916b28ca99ca7a1314f8d264ec2161c
ayumuSasagawa <a_sasagawa@uluru.jp>
2021-05-07 16:40:49 +0900

    Add 課題1/step5_5-4.js

---
+++ 課題1/step5_5-4.js
@@ -0,0 +1,14 @@
+'use strict';
+var fruits = [
+  {name: "バナナ", price: 200},
+  {name: "リンゴ", price: 200},
+  {name: "イチゴ", price: 500},
+  {name: "オレンジ", price: 250},
+  {name: "モモ", price: 350}
+]
+
+var sumPrice = 0;
+for (var i = 0; i<fruits.length; i++){
+  sumPrice = sumPrice + fruits[i].price;
+}
+console.log(sumPrice);
